52f5a9b174b298dcd8678f6207b275bcf34a2a51
zalbuquerque <zaraalb7@gmail.com>
2026-03-14 19:49:20 -0300

    Creating the menu Creating the const file

---
+++ code/Const.py
@@ -0,0 +1,14 @@
+# C
+C_ORANGE = (255, 120, 0)
+C_WHITE = (255, 255, 255)
+C_YELLOW = (255, 255, 0)
+
+# M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+# W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
--- code/Game.py
+++ code/Game.py
@@ -2,15 +2,18 @@
 # -*- coding: utf-8 -*-
 import pygame
 
+from code.Const import WIN_WIDTH, WIN_HEIGHT
 from code.Menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
+
+
         while True:
             menu = Menu(self.window)
             menu.run()
